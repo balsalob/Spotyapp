08d5451271746a383ca356fd7457d8478dcb66a0
balsalob <prietobalsalobre@gmail.com>
2017-03-15 10:44:58 +0100

    Aplicacion entera

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { APP_ROUTING } from './app.routes';
+
+//servicios
+import { SpotifyService } from './services/spotify.service';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/search/navbar/navbar.component';
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent,
+    SinfotoPipe,
+    ArtistaComponent,
+    DomseguroPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
+  ],
+  providers: [
+    SpotifyService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
